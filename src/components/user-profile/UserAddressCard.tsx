b9643acda390e58968da88bda10dda0d6ebd44d9
tomy-cyber <tomyericson1987@gmail.com>
2025-04-06 13:45:50 -0700

    Update UserAddressCard.tsx

--- src/components/user-profile/UserAddressCard.tsx
+++ src/components/user-profile/UserAddressCard.tsx
@@ -52,7 +52,7 @@ export default function UserAddressCard() {
   async function fetchUser() {
     try {
       setLoading(true)
-      const response = await fetch("https://f-backend-l4sd.vercel.app/api/users")
+      const response = await fetch("https://back-end-f.vercel.app/api/users")
       const data = await response.json()
       //setUser(data);
 
