b4693f37b53e9705ac4f4a0ea122135b84916ad6
tomy-cyber <tomyericson1987@gmail.com>
2025-04-06 14:10:16 -0700

    Delete src directory

--- src/app/signup/pages.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "@/components/auth/SignUpForm";
-
-export default function SignupPage() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <SignUpForm />
-    </div>
-  );
-}
