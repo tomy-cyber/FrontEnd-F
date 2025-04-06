1b36f9972ba7bdf2b267c189fadfd50f94cedf28
tomy-cyber <tomyericson1987@gmail.com>
2025-04-06 13:43:28 -0700

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const verifyToken = async (token) => {
   try {
-    const res = await axios.post("https://f-backend-l4sd.vercel.app/api/verify-token", { token });
+    const res = await axios.post("https://back-end-f.vercel.app/api/verify-token", { token });
     return res.data;
   } catch (error) {
     return null;
