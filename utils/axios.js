72ced4607ca931426aed9ef375d06106d5112651
tomy-cyber <tomyericson1987@gmail.com>
2025-04-06 14:10:40 -0700

    Delete utils directory

--- utils/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'https://back-end-f.vercel.app/api',
-  headers: { 'Content-Type': 'application/json' },
-});
-
-export default API;
