1a5bb8b20005446db8d38eee94b5d31a4d758b4b
tomy-cyber <tomyericson1987@gmail.com>
2025-04-14 06:03:17 -0700

    Update StatisticsChart.tsx

--- src/components/ecommerce/StatisticsChart.tsx
+++ src/components/ecommerce/StatisticsChart.tsx
@@ -119,32 +119,32 @@ export default function StatisticsChart() {
       data: [40, 30, 50, 40, 55, 40, 70, 100, 110, 120, 150, 140],
     },
   ];
-  return (
-    <div className="rounded-2xl border border-gray-200 bg-white px-5 pb-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
-      <div className="flex flex-col gap-5 mb-6 sm:flex-row sm:justify-between">
-        <div className="w-full">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
-            Statistics
-          </h3>
-          <p className="mt-1 text-gray-500 text-theme-sm dark:text-gray-400">
-            Target you’ve set for each month
-          </p>
-        </div>
-        <div className="flex items-start w-full gap-3 sm:justify-end">
-          <ChartTab />
-        </div>
-      </div>
+  // return (
+  //   <div className="rounded-2xl border border-gray-200 bg-white px-5 pb-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
+  //     <div className="flex flex-col gap-5 mb-6 sm:flex-row sm:justify-between">
+  //       <div className="w-full">
+  //         <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
+  //           Statistics
+  //         </h3>
+  //         <p className="mt-1 text-gray-500 text-theme-sm dark:text-gray-400">
+  //           Target you’ve set for each month
+  //         </p>
+  //       </div>
+  //       <div className="flex items-start w-full gap-3 sm:justify-end">
+  //         <ChartTab />
+  //       </div>
+  //     </div>
 
-      <div className="max-w-full overflow-x-auto custom-scrollbar">
-        <div className="min-w-[1000px] xl:min-w-full">
-          <ReactApexChart
-            options={options}
-            series={series}
-            type="area"
-            height={310}
-          />
-        </div>
-      </div>
-    </div>
-  );
+  //     <div className="max-w-full overflow-x-auto custom-scrollbar">
+  //       <div className="min-w-[1000px] xl:min-w-full">
+  //         <ReactApexChart
+  //           options={options}
+  //           series={series}
+  //           type="area"
+  //           height={310}
+  //         />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
