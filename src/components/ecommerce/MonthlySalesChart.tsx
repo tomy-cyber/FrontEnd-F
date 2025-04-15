9d9438546bfddeb3c35cb41700e063bd1b49b885
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 05:07:08 -0700

    Update MonthlySalesChart.tsx

--- src/components/ecommerce/MonthlySalesChart.tsx
+++ src/components/ecommerce/MonthlySalesChart.tsx
@@ -115,9 +115,9 @@ export default function MonthlySalesChart() {
         </h3>
 
         <div className="relative inline-block">
-          <button onClick={toggleDropdown} className="dropdown-toggle">
+{/*           <button onClick={toggleDropdown} className="dropdown-toggle">
             <MoreDotIcon className="text-gray-400 hover:text-gray-700 dark:hover:text-gray-300" />
-          </button>
+          </button> */}
           <Dropdown
             isOpen={isOpen}
             onClose={closeDropdown}
