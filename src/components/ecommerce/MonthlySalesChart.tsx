f6c50f30ef032650879fff98de8fce84c379ce95
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 06:03:48 -0700

    Update MonthlySalesChart.tsx

--- src/components/ecommerce/MonthlySalesChart.tsx
+++ src/components/ecommerce/MonthlySalesChart.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { ApexOptions } from "apexcharts";
 import dynamic from "next/dynamic";
-import { MoreDotIcon } from "@/icons";
+// import { MoreDotIcon } from "@/icons";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
-import { useState } from "react";
+// import { useState } from "react";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 
 // Dynamically import the ReactApexChart component
