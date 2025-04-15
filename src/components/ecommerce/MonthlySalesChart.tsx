14bb98befcb6be2c81511a130d1e60468cea6957
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 01:17:18 -0700

    Update MonthlySalesChart.tsx

--- src/components/ecommerce/MonthlySalesChart.tsx
+++ src/components/ecommerce/MonthlySalesChart.tsx
@@ -93,7 +93,7 @@ export default function MonthlySalesChart() {
   };
   const series = [
     {
-      name: "Sales",
+      name: "Case",
       data: [168, 385, 201, 298, 187, 195, 291, 110, 215, 390, 280, 112],
     },
   ];
