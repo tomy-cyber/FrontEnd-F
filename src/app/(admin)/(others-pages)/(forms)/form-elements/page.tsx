4b7bb9a2b0306ec7e431247949e0932877275b2c
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 15:34:40 -0700

    Update page.tsx

--- src/app/(admin)/(others-pages)/(forms)/form-elements/page.tsx
+++ src/app/(admin)/(others-pages)/(forms)/form-elements/page.tsx
@@ -13,9 +13,10 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js Form Elements | TailAdmin - Next.js Dashboard Template",
+    title: "CryptoVault",
   description:
-    "This is Next.js Form Elements page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    "This is CryptoVault Basic Table  page for Userse",
+  // other metadata
 };
 
 export default function FormElements() {
