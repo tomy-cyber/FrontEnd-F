e174492697bd60fbf48eac426d38fced474f659b
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 05:06:42 -0700

    Update DemographicCard.tsx

--- src/components/ecommerce/DemographicCard.tsx
+++ src/components/ecommerce/DemographicCard.tsx
@@ -31,9 +31,9 @@ export default function DemographicCard() {
         </div>
 
         <div className="relative inline-block">
-          <button onClick={toggleDropdown} className="dropdown-toggle">
+{/*           <button onClick={toggleDropdown} className="dropdown-toggle">
             <MoreDotIcon className="text-gray-400 hover:text-gray-700 dark:hover:text-gray-300" />
-          </button>
+          </button> */}
           <Dropdown
             isOpen={isOpen}
             onClose={closeDropdown}
