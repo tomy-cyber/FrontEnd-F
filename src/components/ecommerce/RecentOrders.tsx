c5ab8d88d6c992bc9f1ac0ef07218fbb0a84a039
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 01:13:01 -0700

    Update RecentOrders.tsx

--- src/components/ecommerce/RecentOrders.tsx
+++ src/components/ecommerce/RecentOrders.tsx
@@ -79,7 +79,7 @@ export default function RecentOrders() {
           </h3>
         </div>
 
-        <div className="flex items-center gap-3">
+{/*         <div className="flex items-center gap-3">
           <button className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
             <svg
               className="stroke-current fill-white dark:fill-gray-800"
@@ -121,7 +121,7 @@ export default function RecentOrders() {
           <button className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
             See all
           </button>
-        </div>
+        </div> */}
       </div>
       <div className="max-w-full overflow-x-auto">
         <Table>
