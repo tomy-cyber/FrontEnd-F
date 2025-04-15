48ea0dc2452693d74f7f4a3890a9c389d29cfea5
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 15:35:27 -0700

    Update page.tsx

--- src/app/(admin)/(others-pages)/calendar/page.tsx
+++ src/app/(admin)/(others-pages)/calendar/page.tsx
@@ -4,9 +4,9 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js Calender | TailAdmin - Next.js Dashboard Template",
+    title: "CryptoVault",
   description:
-    "This is Next.js Calender page for TailAdmin  Tailwind CSS Admin Dashboard Template",
+    "This is CryptoVault Basic Table  page for Userse",
   // other metadata
 };
 export default function page() {
