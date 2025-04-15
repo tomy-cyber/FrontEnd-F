4d7635c426e4fa5da184c7d44ed9e59df09cc65b
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 02:32:36 -0700

    Update AppSidebar.tsx

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -59,38 +59,38 @@ const navItems: NavItem[] = [
     ],
   },
 ];
+const othersItems: NavItem[] = [];
+// const othersItems: NavItem[] = [
+// //   {
+// //     icon: <PieChartIcon />,
+// //     name: "Charts",
+// //     subItems: [
+// //       { name: "BTC Price encrease", path: "/line-chart", pro: false },
+// //       { name: "ETH Chart", path: "/bar-chart", pro: false },
+// //     ],
+// //   },
+//   // {
+//   //   icon: <BoxCubeIcon />,
+//   //   name: "UI Elements",
+//   //   subItems: [
+//   //     { name: "Alerts", path: "/alerts", pro: false },
+//   //     { name: "Avatar", path: "/avatars", pro: false },
+//   //     { name: "Badge", path: "/badge", pro: false },
+//   //     { name: "Buttons", path: "/buttons", pro: false },
+//   //     { name: "Images", path: "/images", pro: false },
+//   //     { name: "Videos", path: "/videos", pro: false },
+//   //   ],
+//   // },
+//   // {
+//   //   icon: <PlugInIcon />,
+//   //   name: "Authentication",
+//   //   subItems: [
+//   //     { name: "Sign In", path: "/signin", pro: false },
+//   //     { name: "Sign Up", path: "/signup", pro: false },
+//   //   ],
+//   // },
 
-const othersItems: NavItem[] = [
-//   {
-//     icon: <PieChartIcon />,
-//     name: "Charts",
-//     subItems: [
-//       { name: "BTC Price encrease", path: "/line-chart", pro: false },
-//       { name: "ETH Chart", path: "/bar-chart", pro: false },
-//     ],
-//   },
-  // {
-  //   icon: <BoxCubeIcon />,
-  //   name: "UI Elements",
-  //   subItems: [
-  //     { name: "Alerts", path: "/alerts", pro: false },
-  //     { name: "Avatar", path: "/avatars", pro: false },
-  //     { name: "Badge", path: "/badge", pro: false },
-  //     { name: "Buttons", path: "/buttons", pro: false },
-  //     { name: "Images", path: "/images", pro: false },
-  //     { name: "Videos", path: "/videos", pro: false },
-  //   ],
-  // },
-  // {
-  //   icon: <PlugInIcon />,
-  //   name: "Authentication",
-  //   subItems: [
-  //     { name: "Sign In", path: "/signin", pro: false },
-  //     { name: "Sign Up", path: "/signup", pro: false },
-  //   ],
-  // },
-  return null;
-];
+// ];
 
 const AppSidebar: React.FC = () => {
   const { isExpanded, isMobileOpen, isHovered, setIsHovered } = useSidebar();
