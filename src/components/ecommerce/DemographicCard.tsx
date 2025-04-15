684b8c45d0f0f458722543dfb19b2fd87a883b2c
tomy-cyber <tomyericson1987@gmail.com>
2025-04-15 05:09:52 -0700

    Update DemographicCard.tsx

--- src/components/ecommerce/DemographicCard.tsx
+++ src/components/ecommerce/DemographicCard.tsx
@@ -8,15 +8,15 @@ import { Dropdown } from "../ui/dropdown/Dropdown";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
 
 export default function DemographicCard() {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  function toggleDropdown() {
-    setIsOpen(!isOpen);
-  }
+  // function toggleDropdown() {
+  //   setIsOpen(!isOpen);
+  // }
 
-  function closeDropdown() {
-    setIsOpen(false);
-  }
+  // function closeDropdown() {
+  //   setIsOpen(false);
+  // }
 
   return (
     <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] sm:p-6">
